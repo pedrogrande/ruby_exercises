51fb394c440a0026b329768fdb5390b14a840b67
pedroargento <peter.argent@gmail.com>
2013-11-19 18:20:31 +1100

    ruby exercise 15

---
+++ ex1.rb
@@ -0,0 +1,3 @@
+puts "Hello World!"
+puts "Hello again"
+puts "I like typing this"
---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you? "
+age = get.chomp()
+print "How tall are you? "
+height = get.chomp()
+print "How much do you weight? "
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} tall and weigh #{weight}."
